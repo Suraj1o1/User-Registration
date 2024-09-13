fc6584b4f5f36771baee133a1be26e448e54d6ca
Suraj1o1 <86189592+Suraj1o1@users.noreply.github.com>
2024-09-13 22:35:11 +0545

    fixed error and removed unwanted imports

--- src/component/MainPage.tsx
+++ src/component/MainPage.tsx
@@ -1,9 +1,4 @@
 import React, { use, useState } from 'react'
-import Creatorscard from '@/Dashboard/CreatorsCard'
-import Navbar from './Navbar'
-import SocialPlatform from '@/Dashboard/SocialPlatform'
-import LandingPage from '@/MovieRecomedation/LandingPage'
-import GetApi from '@/API/getApi'
 import RegistrationForm from '@/RegistrationForm/RegistrationForm'
 const MainPage = () => {
   return (
